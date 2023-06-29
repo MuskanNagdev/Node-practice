6008ea8a45afbd21e3898fb27956c4c734f9357a
MuskanNagdev <muskan.nagdev12@gmail.com>
2023-06-30 00:54:25 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+//this type of import system is known as common js module. here the second.js is common js module. here
+const Muskan = require("./second")
+console.log("5 Muskan", Muskan);
+
+// modeule wrapper function
+// node js automatically wrapss the function 
